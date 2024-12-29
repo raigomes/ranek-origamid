98106785fffc45575196c86c1e0d6c6e3951deaf
Raí Mesquita <rmesquita@ogmaster.local>
2024-12-29 11:55:45 -0300

    Add Contato component

---
+++ src/components/Contato.jsx
@@ -0,0 +1,21 @@
+import styles from './Contato.module.css'
+import Head from './Head'
+
+const Contato = () => {
+  return (
+    <section className={styles.contato}>
+      <Head title="Ranek | Contato" description="Curso React Completo | Origamid" />
+      <img src='https://avatars.githubusercontent.com/u/3885994?v=4' />
+      <section className={styles.dados}>
+        <h1>Entre em contato.</h1>
+        <ul>
+          <li>raigomes2@hotmail.com</li>
+          <li>(21) 98019-9439</li>
+          <li>Rio de Janeiro/RJ</li>
+        </ul>
+      </section>
+    </section>
+  )
+}
+
+export default Contato
