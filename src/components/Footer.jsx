567c07f336ebaaa5d3fb7e831c2b8aafef0da10f
Raí Mesquita <rmesquita@ogmaster.local>
2024-12-29 11:34:30 -0300

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        Alguns direitos reservados
+    </footer>
+  )
+}
+
+export default Footer
