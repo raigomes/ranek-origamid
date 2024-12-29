986de04ccb74432e1d96de93940e89ea895db9fa
Raí Mesquita <rmesquita@ogmaster.local>
2024-12-29 11:30:54 -0300

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <ul>
+        <li>
+          <NavLink className={styles.link} to="/" end>Produtos</NavLink>
+        </li>
+        <li>
+          <NavLink className={styles.link} to="contato">Contato</NavLink>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
