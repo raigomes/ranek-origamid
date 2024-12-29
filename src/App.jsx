a69fef89622c151ba24a0844361a981850895f09
Raí Mesquita <rmesquita@ogmaster.local>
2024-12-29 13:17:10 -0300

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import "./App.css"
+
+import Contato from './components/Contato'
+import Produtos from './components/Produtos'
+import Produto from './components/Produto'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Page404 from './components/Page404'
 
 // Utilize a API abaixo para puxar a lista de produto
 // https://ranekapi.origamid.dev/json/api/produto
@@ -7,7 +14,22 @@ import "./App.css"
 // https://ranekapi.origamid.dev/json/api/produto/notebook
 
 function App() {
-  return <h1>ranek-origamid</h1>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path='produto/:id' element={<Produto />} />
+            <Route path="contato" element={<Contato />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
